040e2da3df03b20cf96ccd9072fba569f8a60a57
unknown <saljammal995@gmail.com>
2023-05-14 14:18:18 +0300

    add images slider

--- routes/NavigationStack.js
+++ routes/NavigationStack.js
@@ -1,5 +1,7 @@
+import react, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Image, Text, View } from "react-native";
 import Home from "../screens/Home";
 import About from "../screens/About";
 import Register from "../screens/Register";
@@ -8,15 +10,50 @@ import Profile from "../screens/Profile";
 
 const Stack = createNativeStackNavigator();
 
+const OoredooIcon = () => {
+  return (
+    <View>
+      <Image
+        source={require("../assets/images/Ooredoo-logo-red.png")}
+        style={{ width: 100, height: 100, resizeMode: "contain" }}
+      />
+    </View>
+  );
+};
+
 export default function NavigationStack() {
+  const [headerColor, setHeaderColor] = useState("#fff");
+
+  const changeHeaderColor = (color) => {
+    setHeaderColor(color);
+  };
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            flexDirection: "row",
+            justifyContent: "center",
+            backgroundColor: headerColor,
+          },
+          headerShadowVisible: false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerTitle: (props) => <OoredooIcon {...props} />
+          }}
+        ></Stack.Screen>
         <Stack.Screen name="About" component={About} />
         <Stack.Screen name="Register333" component={Register} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Profile" component={Profile} options={({route}) => ({title: route.params.name})} />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={({ route }) => ({ title: route.params.name })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,37 @@
-import {StyleSheet, View } from "react-native";
-import OoredooIcon from "../components/OoredooIcon";
+import react, { useState } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import { SliderBox } from "react-native-image-slider-box";
 
-export default function Home({ navigation }) {
+export default function Home({ navigation}) {
+  const sliderImages = [
+    require("../assets/images/ooredoo-slider-01.jpg"),
+    require("../assets/images/ooredoo-slider-02.jpg"),
+    require("../assets/images/ooredoo-slider-03.jpg"),
+  ];
+
+  const changeColor = (index) => {
+    if(index === 0){
+     
+      return;
+    }
+    
+  }
   return (
     <View style={styles.wrapper}>
-      <OoredooIcon/>
+      <SliderBox
+        images={sliderImages}
+        dotColor="red"
+        sliderBoxHeight={600}
+        autoplay={true}
+        resizeMode="cover"
+        currentImageEmitter={(index) => changeColor(index)}
+      />
     </View>
   );
 }
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "white",
   },
 });
